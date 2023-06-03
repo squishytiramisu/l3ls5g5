4106b85d3f5bb0e00b238e24bf721bfa13046d42
squishytiramisu <60038184+squishytiramisu@users.noreply.github.com>
2023-06-03 22:13:30 +0200

    Update start_relay1.sh

--- scripts/start_relay1.sh
+++ scripts/start_relay1.sh
@@ -3,12 +3,13 @@
 ./target/release/polkadot \
 --alice \
 --validator \
---rpc-external \
---rpc-port 9933 \
---ws-external \
---rpc-methods Unsafe \
---rpc-cors all \
 --base-path /tmp/relay/alice \
 --chain ./rawroco.json \
+--rpc-port 9933 \
 --port 30333 \
---ws-port 9944
+--ws-port 9944 \
+--rpc-external \
+--ws-external \
+--rpc-methods Unsafe \
+--rpc-cors all
+
